0757dcd22994c0c19b8b0dfd3f21fbc53513cc24
ilpadre <ilpadre1953@gmail.com>
2018-09-07 09:39:17 -0400

    Added project shells

---
+++ webhooks/WebhookReceive/WebhookReceive.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebhookReceive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
